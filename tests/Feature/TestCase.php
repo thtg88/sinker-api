8b10717bd4fc2026167d2db8870e467223472d23
Marco Marassi <thtg88@users.noreply.github.com>
2021-05-31 15:27:15 +0100

    Add loginAs method to base feature test case

--- tests/Feature/TestCase.php
+++ tests/Feature/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase as BaseTestCase;
@@ -10,4 +11,22 @@ abstract class TestCase extends BaseTestCase
 {
     use RefreshDatabase;
     use WithFaker;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()->create();
+
+        $this->user->devices()->create();
+    }
+
+    public function loginAs(User $user): self
+    {
+        return $this->withHeaders([
+            config('app.auth_headers.api_key') => $user->api_key,
+            config('app.auth_headers.device_id') => $user->devices->first()->uuid,
+            config('app.auth_headers.user_id') => $user->uuid,
+        ]);
+    }
 }
