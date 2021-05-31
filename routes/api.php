f6e0bc09861bc9e31327023fc34c7006ee639e01
Marco Marassi <thtg88@users.noreply.github.com>
2021-05-31 16:55:20 +0100

    Removed latest events route

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,8 @@
 <?php
 
-use App\Http\Controllers\LatestEventController;
 use App\Http\Controllers\Event\StoreController as StoreEventController;
 use Illuminate\Support\Facades\Route;
 
-    Route::get('latest', LatestEventController::class)->name('latest');
 Route::group(['middleware' => 'auth:api', 'prefix' => 'v1', 'as' => 'v1.'], static function () {
     Route::group(['middleware' => 'authorize_device'], static function () {
         Route::group(['prefix' => 'events', 'as' => 'events.'], static function () {
